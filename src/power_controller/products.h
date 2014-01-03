b4d32a60a05e64b3abe3736b243aadbcc3be1a66
Amoriello Hutti <amoriello.hutti@gmail.com>
2014-01-04 00:44:13 +0100

    Removing intermediate ProductId and Vendor Id structures.
    User input validation :
    Adding a nb_outlet attribute and IsValidOutlet inside PowerStrip strucutre.
    
    @todo (Amoriello) :
    Update the nb_outlet for other products.

--- src/power_controller/products.h
+++ src/power_controller/products.h
@@ -6,41 +6,41 @@
 
 #include <cstdint>
 
-struct VendorId {
-  uint16_t value;
-};
-
-struct ProductId {
-  uint16_t value;
-};
-
 
 struct PowerStrip {
-  VendorId vendor_id;
-  ProductId product_id;
+  uint16_t vendor_id;
+  uint16_t product_id;
+  uint8_t nb_outlet;
+
+  bool IsValidOutlet(uint8_t outlet) const {
+    return (outlet > 0) && (outlet <= nb_outlet);
+  }
 };
 
 
 namespace products {
 static PowerStrip kSisPm{
-  VendorId{ 0x04b4 },
-  ProductId{ 0xfd11 }
+  0x04b4,  // vendor_id
+  0xfd11,  // product_id
+  1        // nb_outlet
 };
 
-
 static PowerStrip kMSisPmOld {
-  VendorId{ 0x04b4 },
-  ProductId{ 0xfd11 }
+  0x04b4,  // vendor_id
+  0xfd11,  // product_id
+  1        // nb_outlet
 };
 
 static PowerStrip kMSisPmFlash {
-  VendorId{ 0x04b4 },
-  ProductId{ 0xfd11 }
+  0x04b4,  // vendor_id
+  0xfd11,  // product_id
+  1
 };
 
 static const PowerStrip kSisPmFlashNew {
-  VendorId{ 0x04b4 },
-  ProductId{ 0xfd13 }
+  0x04b4,   // vendor_id
+  0xfd13,   // product_id
+  4         // nb_outlet
 };
 }  // namespace products
 
